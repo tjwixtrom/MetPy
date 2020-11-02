95203a9f2f1f892cd72ab35d398c202c86c98d9c
tjwixtrom <tjwixtrom@users.noreply.github.com>
2020-11-02 21:56:24 +0000

    Deploy dev to GitHub Pages

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post447+g86883d4',
+    VERSION: '0.0.post583+gcde84bb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
